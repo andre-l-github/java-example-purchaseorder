5640dc77966e267a6efb7c2935f46b428a36780d
andre <andre.lahs.mail@googlemail.com>
2014-08-09 17:46:56 +0200

    Setup camunda embedded bpm engine

--- src/main/java/com/lahs/examples/purchaseorder/Application.java
+++ src/main/java/com/lahs/examples/purchaseorder/Application.java
@@ -9,6 +9,6 @@ import org.springframework.context.annotation.ComponentScan;
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        new SpringApplication(Application.class, CamundaConfig.class).run(args);
     }
 }
---
+++ src/main/java/com/lahs/examples/purchaseorder/CamundaConfig.java
@@ -0,0 +1,11 @@
+package com.lahs.examples.purchaseorder;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+
+@Configuration
+@ImportResource({"classpath:camunda-spring.xml"})   
+public class CamundaConfig {
+
+}
